62a229e842bc202e17c6ad2fb3453be253edfee2
Victoria Orlova <victoria1orlova@gmail.com>
2020-04-18 20:23:17 +0300

    Added english mind map

---
+++ English.mm
@@ -0,0 +1,54 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1587226465646" ID="ID_975995512" MODIFIED="1587226512904" TEXT="English">
+<node CREATED="1587226694760" ID="ID_1418205736" MODIFIED="1587227253733" POSITION="right" TEXT="Present simple">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Do,does
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1587226764703" ID="ID_1938946634" MODIFIED="1587226781987" TEXT="I do it"/>
+<node CREATED="1587226783514" ID="ID_1649261889" MODIFIED="1587226859341" TEXT="You do it. Don&apos;t you?"/>
+<node CREATED="1587226862334" ID="ID_590083292" MODIFIED="1587227755524" TEXT="What do you do now?"/>
+</node>
+<node CREATED="1587227298729" ID="ID_1771137534" MODIFIED="1587227335399" POSITION="left" TEXT="Past simple">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Did
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1587227365169" ID="ID_1536226847" MODIFIED="1587227415898" TEXT="You did it!"/>
+<node CREATED="1587227425106" ID="ID_1058223525" MODIFIED="1587227522273" TEXT="You did it. Didn&apos;t you?"/>
+<node CREATED="1587227522635" ID="ID_1728713608" MODIFIED="1587227610825" TEXT="You didn&apos;t do it. Did you?"/>
+</node>
+<node CREATED="1587227790111" ID="ID_1491933218" MODIFIED="1587228378323" POSITION="right" TEXT="Present continues">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      am
+    </p>
+    <p>
+      is
+    </p>
+    <p>
+      are
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="ksmiletris"/>
+</node>
+</node>
+</map>
