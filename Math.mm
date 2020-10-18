0ce869d322fa6bab46d565f66d18d086bcadb609
VikaPaw0990 <victoria1orlova@gmail.com>
2020-10-18 05:29:18 -0700

    I started my lessons with mathematics

---
+++ Math.mm
@@ -0,0 +1,16 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1602942166072" ID="ID_1501701530" MODIFIED="1602942254295" TEXT="Math">
+<node CREATED="1602942261535" ID="ID_1237867074" MODIFIED="1602942821045" POSITION="right" TEXT="&#x427;&#x438;&#x441;&#x43b;&#x430;">
+<node CREATED="1602942357193" ID="ID_413794630" MODIFIED="1602942369305" TEXT="&#x41f;&#x43e;&#x447;&#x451;&#x442;&#x43d;&#x43e;&#x441;&#x442;&#x438;">
+<node CREATED="1602942375285" ID="ID_1312808740" MODIFIED="1602942392660" TEXT="&#x427;&#x451;&#x442;&#x43d;&#x44b;&#x435;"/>
+<node CREATED="1602942394105" ID="ID_655509949" MODIFIED="1602942418108" TEXT="&#x41d;&#x435;&#x447;&#x451;&#x442;&#x43d;&#x44b;&#x435;"/>
+</node>
+<node CREATED="1602942784806" MODIFIED="1602942784806" TEXT="&#x41d;&#x430;&#x442;&#x443;&#x440;&#x430;&#x43b;&#x44c;&#x43d;&#x44b;&#x435; &#x447;&#x438;&#x441;&#x43b;&#x430;"/>
+<node CREATED="1602942784806" MODIFIED="1602942784806" TEXT="&#x426;&#x435;&#x43b;&#x44b;&#x435; &#x447;&#x438;&#x441;&#x43b;&#x430;"/>
+<node CREATED="1602942784806" MODIFIED="1602942784806" TEXT="&#x420;&#x430;&#x446;&#x438;&#x43e;&#x43d;&#x430;&#x43b;&#x44c;&#x43d;&#x44b;&#x435; &#x447;&#x438;&#x441;&#x43b;&#x430;"/>
+<node CREATED="1602942784806" MODIFIED="1602942784806" TEXT="&#x414;&#x435;&#x439;&#x441;&#x442;&#x432;&#x438;&#x442;&#x435;&#x43b;&#x44c;&#x43d;&#x44b;&#x435; (&#x432;&#x435;&#x449;&#x435;&#x441;&#x442;&#x432;&#x435;&#x43d;&#x43d;&#x44b;&#x435;) &#x447;&#x438;&#x441;&#x43b;&#x430;"/>
+<node CREATED="1602942784806" MODIFIED="1602942784806" TEXT="&#x41a;&#x43e;&#x43c;&#x43f;&#x43b;&#x435;&#x43a;&#x441;&#x43d;&#x44b;&#x435; &#x447;&#x438;&#x441;&#x43b;&#x430;"/>
+</node>
+</node>
+</map>
